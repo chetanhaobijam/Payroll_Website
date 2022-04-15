45175571ea2869d883e2f72cd3e485c79a7542f7
Haobijam Chetan Singh <67411987+chetanhaobijam@users.noreply.github.com>
2022-04-15 17:01:34 +0530

    Finished Basic Styling

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+const navMenu = document.querySelector(".nav-menu i");
 const navHeader = document.querySelector(".nav-header h1");
+const sidebar = document.querySelector(".sidebar");
 const adminSidebar = document.querySelector(".admin-sidebar");
 const publicSidebar = document.querySelector(".public-sidebar");
+const sideTitle = document.querySelectorAll(".side-title");
+
 
 window.addEventListener("keyup", (event) => {
   if(event.key === "p") {
@@ -12,4 +16,11 @@ window.addEventListener("keyup", (event) => {
     publicSidebar.classList.add("d-none");
     adminSidebar.classList.remove("d-none");
   }
+})
+
+navMenu.addEventListener("click", () => {
+  sidebar.classList.toggle("short-width");
+  sideTitle.forEach(title => {
+    title.classList.toggle("d-none");
+  })
 })
