a2e12076f2ad2f5a6b6deb64ad0be0b4699a98d2
Haobijam Chetan Singh <chetanhaobijam@gmail.com>
2022-04-21 15:50:11 +0530

    Added Navbar

--- main.js
+++ main.js
@@ -1,26 +1,12 @@
-const navMenu = document.querySelector(".nav-menu i");
-const navBrand = document.querySelector(".nav-brand h1");
-const sidebar = document.querySelector(".sidebar");
-const adminSidebar = document.querySelector(".admin-sidebar");
-const publicSidebar = document.querySelector(".public-sidebar");
-const sideTitle = document.querySelectorAll(".side-title");
+const navbarToggler = document.querySelector("#navbar-toggler");
+const main = document.querySelector(".main");
+const footer = document.querySelector("footer");
 
-
-window.addEventListener("keyup", (event) => {
-  if(event.key === "p") {
-    navBrand.innerText = "Dashboard";
-    publicSidebar.classList.remove("d-none");
-    adminSidebar.classList.add("d-none");
-  } else if(event.key === "a") {
-    navBrand.innerText = "Admin Dashboard";
-    publicSidebar.classList.add("d-none");
-    adminSidebar.classList.remove("d-none");
-  }
-})
-
-navMenu.addEventListener("click", () => {
-  sidebar.classList.toggle("short-width");
-  sideTitle.forEach(title => {
-    title.classList.toggle("d-none");
+const narrowItems = () => {
+  navbarToggler.addEventListener("click", () => {
+    main.classList.toggle("narrow-main");
+    footer.classList.toggle("narrow-footer");
   })
-})
+}
+
+narrowItems();
